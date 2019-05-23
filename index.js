4f4c422fcc54db566b43891a134799a0f47f18bc
neel <neel2292@gmail.com>
2019-05-23 10:01:52 +0800

    Better error handling

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ module.exports = function (opts) {
             obj.mode = opts.precision;
             obj.str = file.contents.toString('utf8');
 
-            nano.compressString(obj, opts).then(function (file) {
+            nano.compressString(obj, opts, 2).then(function (file) {
                 console.log('Compressed: ' + file.name + ' ' + //eslint-disable-line no-console
                     ' ' + (file.old_size / 1024).toFixed(1) + 'KB -> ' + (file.size /1024).toFixed(1) + 'KB' +
                     ' ' + (((file.old_size - file.size) / file.old_size) * 100).toFixed(2) + '% saved');
